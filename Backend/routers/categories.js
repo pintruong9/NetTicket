843ade47a8ebe2852b9235c0863f510e510f69e8
Truong Pin <pintruongaus3979@gmail.com>
2026-02-20 01:00:21 +0700

    add product and category

--- Backend/routers/categories.js
+++ Backend/routers/categories.js
@@ -4,5 +4,9 @@ const controller = require("../controllers/categories");
 
 router.get("/", controller.getCategories);
 console.log("[LOG] GET /api/categories ");
+router.get("/:id", controller.getCategoryId);
+router.post("/", controller.createCategory);
+router.put("/:id", controller.updateCategory);
+router.delete("/:id", controller.deleteCategory);
 
 module.exports = router;
