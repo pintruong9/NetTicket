77233dd19d72e427c2bb97fb400f7e487f1f31c7
Truong Pin <pintruongaus3979@gmail.com>
2026-02-19 23:50:37 +0700

    add users

--- Backend/routers/users.js
+++ Backend/routers/users.js
@@ -4,6 +4,8 @@ const controller = require("../controllers/users");
 
 router.get("/", controller.getUsers);
 console.log("GET /api/users");
+router.get("/:id", controller.getUserById);
 router.post("/", controller.createUser);
-
+router.put("/:id", controller.updateUser);
+router.delete("/:id", controller.deleteUser);
 module.exports = router;
